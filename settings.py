0fe15294335cbc5dca80562a40ddcc52691a9e4a
rtmagar <username@example.com>
2020-11-21 16:55:43 +0545

    First Commit

---
+++ settings.py
@@ -0,0 +1,60 @@
+class Settings:
+	"""A class to store all settings for Alien Invasion."""
+
+	def __init__(self):
+		"""Initialize the game's settings"""
+		# Screen Window 
+		self.screen_width = 1000 
+		self.screen_height = 600 
+		self.bg_color = (135, 206, 250)
+		
+
+		# Bullet Settings
+		self.bullet_speed = 1.5
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
+		
+		# Alien Settings
+		self.alien_speed = 0.8
+		self.fleet_drop_speed = 10
+
+		# How quickly the game speed up.
+		self.speedup_scale = 1.1
+
+		# How quickly the alien point value increase
+		self.score_scale = 1.5
+
+		self.initialize_dynamic_settings()
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
+
+		# Ship Settings
+		self.ship_speed = 1.5
+		self.ship_limit = 3
+
+	def initialize_dynamic_settings(self):
+		"""Initialize settings that changes throughout the game."""
+		self.ship_speed = 1.5
+		self.bullet_speed = 3.0 
+		self.alien_speed = 1.0
+
+		# fleet_direction of 1 represents right; -1 represents left.
+		self.fleet_direction = 1
+
+		# Scoring
+		self.alien_points = 50
+
+
+	def increase_speed(self):
+		"""Increase speed settings and point values."""
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
+
+		self.alien_points = int(self.alien_points * self.score_scale)
+		print(self.alien_points)
+
+
+
