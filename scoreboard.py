7e280c979007a945090f2b66159d148c45ff7484
rtmagar <username@example.com>
2020-11-22 15:00:42 +0545

    Added gif file

--- scoreboard.py
+++ scoreboard.py
@@ -35,7 +35,8 @@ class Scoreboard:
 
 	def prep_level(self):
 		"""Turn the level into a rendered image."""
-		level_str = str(self.stats.level)
+		lvl = str(self.stats.level)
+		level_str = f'Level: {lvl}'
 		self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
 
 		# Position the level below the score
@@ -46,7 +47,7 @@ class Scoreboard:
 	def prep_high_score(self):
 		"""Turn the high score into a rendered image."""
 		high_score = round(self.stats.high_score, -1)
-		high_score_str = f'{high_score:,}'
+		high_score_str = f'High Score: {high_score:,}'
 		self.high_score_image = self.font.render(high_score_str, True,
 				self.text_color, self.settings.bg_color)
 
@@ -59,7 +60,7 @@ class Scoreboard:
 	def prep_score(self):
 		"""Turn the score into a rendered image."""
 		rounded_score = round(self.stats.score, -1)
-		score_str = f"{rounded_score:,}"
+		score_str = f"Score: {rounded_score:,}"
 		self.score_image = self.font.render(score_str, True, 
 				self.text_color, self.settings.bg_color)
 
